3db1bd982365ff8409322c79f15460e5c419b327
ODCA117 <soa.96@hotmail.com>
2017-01-26 16:50:20 +0100

    Add lab1

---
+++ lab1/src/set/RemoveDuplicates.java
@@ -0,0 +1,29 @@
+package set;
+
+public class RemoveDuplicates {
+
+	//Testing for Github
+	//Hello
+	
+	public static void main(String[] args) {
+		int[] xs = {7,5,3,5,2,2,7};
+		int[] ys = uniqueElements(xs);
+		for(int i: ys){
+			System.out.println(i);
+		}
+	}
+	
+	public static int[] uniqueElements(int[] ints){
+		MaxSet<Integer> ue = new MaxSet();
+		for(int i : ints){
+			ue.add(i);
+		}
+		int[] newInts = new int[ue.size()];
+		for(int i = ue.size()-1; i >= 0; i--){
+			newInts[i] = ue.getMax();
+			ue.remove(ue.getMax());
+		}
+		return newInts;
+	}
+
+}
